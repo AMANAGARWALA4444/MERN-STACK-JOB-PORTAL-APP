84e34b5929a17bbdfa8654dae278657a26e21d2f
AMANAGARWALA4444 <114352573+AMANAGARWALA4444@users.noreply.github.com>
2024-03-27 02:27:47 +0530

    https://github.com/AMANAGARWALA4444/jobPortal

---
+++ client/src/pages/Register.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import InputFrom from "../components/shared/InputFrom";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // form function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      console.log(name, email, password, lastName);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="form-container">
+        <form className="card p-2" onSubmit={handleSubmit}>
+          <img
+            src="/assets/images/logo/logo.png"
+            alt="logo"
+            height={150}
+            width={400}
+          />
+          <InputFrom
+            htmlFor="name"
+            labelText={"Name"}
+            type={"text"}
+            value={name}
+            handleChange={(e) => setName(e.target.value)}
+            name="name"
+          />
+          <InputFrom
+            htmlFor="lastName"
+            labelText={"Last Name"}
+            type={"text"}
+            value={lastName}
+            handleChange={(e) => setLastName(e.target.value)}
+            name="lastName"
+          />
+          <InputFrom
+            htmlFor="email"
+            labelText={"Email"}
+            type={"email"}
+            value={email}
+            handleChange={(e) => setEmail(e.target.value)}
+            name="email"
+          />
+          <InputFrom
+            htmlFor="password"
+            labelText={"Password"}
+            type={"password"}
+            value={password}
+            handleChange={(e) => setPassword(e.target.value)}
+            name="password"
+          />
+
+          <div className="d-flex justify-content-between">
+            <p>
+              Already Register <Link to="/login">Login</Link>{" "}
+            </p>
+            <button type="submit" className="btn btn-primary">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
